6c2ce8c064e9c1a0171f7ae6a8fe603237207748
Jean <jeanncavalcante@gmail.com>
2023-01-02 22:08:53 -0300

    auth

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,17 @@
 import { Router, Request, Response } from "express";
+import * as createUser from "./controllers/CreateController";
+import * as login from "./controllers/AuthController";
 
 const router = Router();
 
-router.get("/", (req, res: Response) => {
+router.get("/", ( res: Response) => {
   return res.json({ message: "Hello World" });
 });
 
+router.get("/hello", createUser.hello);
+
+router.post("/create", createUser.createPost);
+
+router.post("/auth", login.auth);
 
 export { router };
