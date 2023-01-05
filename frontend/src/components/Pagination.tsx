6d8f9d5e3132f8b7b574397421d94f68c7389dc2
Jean <jeanncavalcante@gmail.com>
2023-01-04 21:36:59 -0300

    feat: new page

---
+++ frontend/src/components/Pagination.tsx
@@ -0,0 +1,13 @@
+import { ArrowLeft, ArrowRight } from "phosphor-react";
+
+const Pagination = (props: any) => {
+  console.log(props )
+  return (
+   
+      
+  );
+};
+
+
+
+export default Pagination;
